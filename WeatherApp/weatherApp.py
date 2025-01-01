8414866a5b956d96449381c33481c8c6a331919d
Nidhish Rajan <nidhishraj.nr@gmail.com>
2025-01-01 18:06:25 +0530

    New Project added

---
+++ WeatherApp/weatherApp.py
@@ -0,0 +1,40 @@
+import requests
+
+# Define API keys and base URL for OpenWeatherMap API
+
+api_key = "2d1b21e2ce92ca33027dc5f6f2d69249"
+base_URL = "http://api.openweathermap.org/data/2.5/weather?"
+
+
+# Function to get the weather data
+
+def getWeather(city):
+    # Construct the URL to get the weather data for the specified city
+    url = f"{base_URL}q={city}&appid={api_key}&units=metric"
+
+    # Send a GET request to API
+    response = requests.get(url)
+
+    # Check if the request to the API
+    if response.status_code == 200:
+        data = response.json()
+
+        # Extract weather information from the response
+
+        main = data["main"]
+        weather = data["weather"][0]
+
+        # Display weather details
+        print(f"Weather in {city}:")
+        print(f"Temperature: {main['temp']}°C")
+        print(f"Humidity: {main['humidity']}%")
+        print(f"Weather: {weather["description"]}")
+
+    else:
+        print(f"City {city} not found.")
+
+
+# Main function to run the app
+if __name__ == "__main__":
+    city = input("Enter the city name: ")
+    getWeather(city)
